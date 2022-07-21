c4f471062b0f9828dd3f278ef2964438bc31334d
Alberto Trabucco <alberto.trabucco97@gmail.com>
2022-07-21 22:53:09 +0200

    fine prima parte

--- src/app/components/ordini/ordini.component.ts
+++ src/app/components/ordini/ordini.component.ts
@@ -1,9 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
-import { empty } from 'rxjs';
 import { IOrdine } from 'src/app/models/ordineModel';
 import { ITariffa } from 'src/app/models/tariffeModel';
 import { IVoce } from 'src/app/models/voceModel';
 import { AppService } from 'src/app/services/app.service';
+import { IArticolo } from '../../models/articoloModel';
 
 @Component({
   selector: 'app-ordini',
@@ -16,30 +17,41 @@ export class OrdiniComponent implements OnInit {
   ordini: IOrdine[] = [];
   voci: IVoce[] = [];
   tariffe: ITariffa[] = [];
- 
+  articoli: IArticolo[] = [];
+
   selectedOrder: IOrdine | undefined;
   selectedVoce: IVoce[] = [];
   selectedTariffa: ITariffa | undefined;
-  
+  selectedArticolo: IArticolo | undefined;
+  pesoTotale: number = 0;
+
 
   constructor(private appService: AppService) {
    }
 
   ngOnInit(): void {
-    this.appService.getOrdini().subscribe(data => 
-        this.ordini = data)   
-    this.appService.getVoci().subscribe(data => 
+    this.appService.getOrdini().subscribe(data =>
+        this.ordini = data)
+    this.appService.getVoci().subscribe(data =>
       this.voci = data)
-    this.appService.getTariffe().subscribe(data => 
-      this.tariffe = data)    
+    this.appService.getTariffe().subscribe(data =>
+      this.tariffe = data)
+    this.appService.getArticoli().subscribe(data =>
+      this.articoli = data)
   }
 
-  getTariffaMigliore(id: number){
+  getTariffaMigliore(id: number) : ITariffa | undefined{
     this.selectedOrder = this.ordini.find(x => x.id == id);
     this.selectedVoce = this.voci.filter(x => x.id_ordine == this.selectedOrder?.id);
     for(let i=0; i<this.selectedVoce.length; i++){
-      
+      this.selectedArticolo = this.articoli.find(x => x.id == this.selectedVoce[i].articolo)
+      if(this.selectedVoce[i].articolo == this.selectedArticolo?.id){
+        this.pesoTotale = this.selectedVoce[i].quantità * this.selectedArticolo.peso;
+      }
     }
+    this.selectedTariffa = this.tariffe.find(x => x.peso_massimo >= this.pesoTotale);
+    let tariffaMigliore = this.selectedTariffa;
+    return tariffaMigliore;
   }
 
 
